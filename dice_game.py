8bfe3f6eade0c99231c371c48b74f559eb37bf27
harsh Savaliya <harsh.savaliya@seaflux.tech>
2025-01-22 18:11:55 +0530

    feat: add dice roll game file

---
+++ dice_game.py
@@ -0,0 +1,25 @@
+import random
+
+def roll_dice():
+    return random.randint(1,6)
+
+
+def main():
+    print("\n------Welcome to Dice Game------")
+    while True:
+        print("\nEnter 1 to Roll the Dice.\nEnter 0 for exit.")
+        user_choice=input(">")
+        
+        if user_choice=="1":
+            rolled_number=roll_dice()
+            print(f"\nYou rolled a {rolled_number}.")
+            
+        elif user_choice=="0":
+            print("\n--Thank you for Playing--")
+            break
+        else:
+            print("\nInvalid input!!")
+            
+
+if __name__ == "__main__":
+    main() 
