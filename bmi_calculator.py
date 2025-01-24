32c73a0cbba3cd4843dfaf8d4a21103f1c6741bf
harsh Savaliya <harsh.savaliya@seaflux.tech>
2025-01-24 11:51:28 +0530

    feat: add bmi calculator

---
+++ bmi_calculator.py
@@ -0,0 +1,55 @@
+def BMI_calculator(weight, height):
+    """Calculate BMI and return the corresponding category."""
+    
+    bmi_score= weight / ((height/100)**2)
+    
+    if bmi_score<18.5:
+        return "Underweight"
+    elif bmi_score>=18.5 and bmi_score<25:
+        return "Normal"
+    elif bmi_score>=25 and bmi_score<30:
+        return "Overweight"
+    elif bmi_score>=30:
+        return "Obesity"     
+
+def get_valid_input(prompt, value_type=float):
+    """for user input and validate it."""
+    
+    while True:
+        user_input=input(prompt)
+        try:
+            value=value_type(user_input)
+            
+            if value_type!=str:
+                if value<=0:
+                    print(">>Value should be greater than 0.\n")
+                else:
+                    return value
+            else:
+                if not all(c.isalpha() or c.isspace() for c in user_input.strip()) or len(user_input.strip()) == 0:
+                    print(">> Name should only contain alphabetic characters and spaces, and cannot be empty.\n")
+                else:
+                    return value
+            
+        except ValueError:
+            print(f">>Invalid input. please enter {value_type}value")
+    
+
+def main():
+    """Main function to calculate and display BMI."""
+    
+    print("------BMI Calculator------")
+    print("Fill up the following details:")
+    
+    name=get_valid_input("\nEnter Your Name: ",str) 
+    height=get_valid_input("\nEnter Your Height(in cm): ")
+    weight=get_valid_input("\nEnter Your Weight(in kg): ")
+
+    category=BMI_calculator(weight,height)
+    
+    print(f"\n{name.title()}, your BMI falls under the '{category}' category.")
+
+        
+
+if __name__=="__main__":
+    main()
